c31f7b7ce974550a911580187375674961cd3e52
awaghadpd <74618130+awaghadpd@users.noreply.github.com>
2023-07-28 16:54:10 +0530

    Update configure-ingress-controller1.sh

--- ingress/configure-ingress-controller1.sh
+++ ingress/configure-ingress-controller1.sh
@@ -4,4 +4,5 @@ $NAMESPACE= test
 
 
 # create namespace if doesn't exists
-kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+#kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace $NAMESPACE
