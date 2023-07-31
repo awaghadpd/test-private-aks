c125d9fd2f4d2daa729a6eb1b837bee64fe0665a
awaghadpd <74618130+awaghadpd@users.noreply.github.com>
2023-07-31 12:47:27 +0530

    Update configure-ingress-controller1.sh

--- ingress/configure-ingress-controller1.sh
+++ ingress/configure-ingress-controller1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 NAMESPACE=$3
-AKS_HELLO=$5
+TEST_DEPLOY=$5
 
 
 # create namespace if doesn't exists
 kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
 # Create aks-helloworld and ingress-demo
-kubectl apply -f $AKS_HELLO --namespace $NAMESPACE
+kubectl apply -f $TEST_DEPLOY --namespace $NAMESPACE
